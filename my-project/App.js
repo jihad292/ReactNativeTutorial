fe381e98c93b604f61d60473e3e08fec4d0d2bad
jihad292 <jihad_chamoun1221@outlook.com>
2021-10-14 23:28:36 +0300

    lesson-15

---
+++ my-project/App.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
+
+export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play on the switch', key: '3' },
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  };
+
+  const submitHandler = (text) => {
+    if(text.length > 3){
+      setTodos(prevTodos => {
+        return [
+          { text, key: Math.random().toString() },
+          ...prevTodos
+        ];
+      });
+    } else {
+      Alert.alert('OOPS', 'Todo must be over 3 characters long', [
+        {text: 'Understood', onPress: () => console.log('alert closed') }
+      ]);
+    }
+  };
+
+  return (
+<<<<<<< HEAD
+    // <Sandbox />
+=======
+>>>>>>> 205842b6697bf74c18d78502a468ca26ee743f77
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+      console.log('dismissed');
+    }}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  content: {
+    padding: 40,
+<<<<<<< HEAD
+    //backgroundColor: 'grey',
+=======
+>>>>>>> 205842b6697bf74c18d78502a468ca26ee743f77
+    flex: 1,
+  },
+  list: {
+    marginTop: 20,
+<<<<<<< HEAD
+    //backgroundColor: 'lightgrey',
+=======
+>>>>>>> 205842b6697bf74c18d78502a468ca26ee743f77
+    flex: 1,
+  },
+});
